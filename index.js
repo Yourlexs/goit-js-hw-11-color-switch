d8767191297d2aa0d1dc972ca3be17b1d6d66fd5
Viktoria Alekseeva <Yourlexss@gmail.com>
2021-05-15 12:24:20 +0300

    added files

---
+++ index.js
@@ -0,0 +1,33 @@
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const refs = {
+    startBtn: document.querySelector('button[data-action="start"]'),
+    stopBtn: document.querySelector('button[data-action="stop"]'),
+    body: document.querySelector('body'),
+}
+
+refs.startBtn.addEventListener('click', onChangeBgrColor);
+
+function onChangeBgrColor() {
+    const intervalID = setInterval(() => {
+        refs.body.style.backgroundColor = colors[randomIntegerFromInterval(0, 6)];
+        refs.startBtn.disabled = true;
+    }, 1000);
+
+    refs.stopBtn.addEventListener('click', () => {
+        clearInterval(intervalID);
+        refs.startBtn.disabled = false;
+    });
+}
+
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
